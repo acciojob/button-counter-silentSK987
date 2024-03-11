f7ee9508b8e76ec4e86b8396886c40c2c0fe1887
Pravin Bansod <136956809+silentSK987@users.noreply.github.com>
2024-03-11 20:19:47 +0000

    done again

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,33 @@
 import React from "react";
 import './../styles/App.css';
 
+class Container extends Component {
+  constructor(props) {
+      super(props);
+      this.state = { count: 0 };
+  }
 
+  increamentCount = () => {
+      const curr = this.state.count;
+      this.setState({ count: curr + 1 })
+  }
+  render() {
+      return (
+          <>
+              <p>Button clicked {this.state.count} times</p>
+              <button onClick={this.increamentCount}>Click me</button>
+          </>
 
+      )
+  }
+
+}
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
-      <Counter/>
+      <Container/>
     </div>
   )
 }
